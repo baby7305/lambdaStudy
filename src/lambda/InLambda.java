3e786b531752ac27b9c8068e6b3e67197b2d2407
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-12 15:49:10 +0800

    函数调用带参数lambda，输出结果

--- src/lambda/InLambda.java
+++ src/lambda/InLambda.java
@@ -4,7 +4,5 @@ package lambda;
  * Created by Administrator on 2018/1/12.
  */
 public interface InLambda {
-//	void performed();
-
 	int performed1(int x, int y);
 }
--- src/lambda/LambdaMain.java
+++ src/lambda/LambdaMain.java
@@ -4,15 +4,15 @@ package lambda;
  * Created by Administrator on 2018/1/12.
  */
 public class LambdaMain {
+	public int test(InLambda inLambda) {
+		return inLambda.performed1(1, 2);
+	}
+
 	public static void main(String[] args) {
-//		InLambda inLambda = () -> {
-//			System.out.println("Hello lambda");
-//		};
-//		inLambda.performed();
+		LambdaMain lambdaMain=new LambdaMain();
 		InLambda inLambda = (int x, int y) -> {
 			return x + y;
 		};
-
-		System.out.println(inLambda.performed1(2, 4));
+		System.out.println(lambdaMain.test(inLambda));
 	}
 }
